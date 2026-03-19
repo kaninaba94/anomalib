6d28657c8ee6b568cbc7cc45ddf15d9e6fc3aaae
Kaspar Naraghi <kaninaba94@gmail.com>
2026-03-19 14:19:53 +0100

    Remove intermediate variable to avoid memory spike
    Signed-off-by: Kaspar Naraghi <kaninaba94@gmail.com>

--- src/anomalib/models/image/patchcore/torch_model.py
+++ src/anomalib/models/image/patchcore/torch_model.py
@@ -128,7 +128,6 @@ class PatchcoreModel(DynamicBufferMixin, nn.Module):
         self.anomaly_map_generator = AnomalyMapGenerator()
         self.memory_bank: torch.Tensor
         self.register_buffer("memory_bank", torch.empty(0))
-        self.embedding_store: list[torch.tensor] = []
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor | InferenceBatch:
         """Process input tensor through the model.
@@ -173,7 +172,10 @@ class PatchcoreModel(DynamicBufferMixin, nn.Module):
         embedding = self.reshape_embedding(embedding)
 
         if self.training:
-            self.embedding_store.append(embedding)
+            if self.memory_bank.size(0) == 0:
+                self.memory_bank = embedding
+            else:
+                self.memory_bank = torch.cat([self.memory_bank, embedding], dim=0)
             return embedding
 
         # Ensure memory bank is not empty
@@ -272,14 +274,11 @@ class PatchcoreModel(DynamicBufferMixin, nn.Module):
         if embeddings is not None:
             del embeddings
 
-        if len(self.embedding_store) == 0:
+        if self.memory_bank.size(0) == 0:
             msg = "Embedding store is empty. Cannot perform coreset selection."
             raise ValueError(msg)
 
         # Coreset Subsampling
-        self.memory_bank = torch.vstack(self.embedding_store)
-        self.embedding_store.clear()
-
         sampler = KCenterGreedy(embedding=self.memory_bank, sampling_ratio=sampling_ratio)
         self.memory_bank = sampler.sample_coreset()
 
